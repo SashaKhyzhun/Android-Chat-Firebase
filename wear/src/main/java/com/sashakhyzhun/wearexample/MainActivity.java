3bdae946ff60769b99f7e8cfcb7d5c142af71052
SashaKhyzhun <sasha6427@gmail.com>
2016-10-05 22:42:36 +0300

    Initial commit

---
+++ wear/src/main/java/com/sashakhyzhun/wearexample/MainActivity.java
@@ -0,0 +1,20 @@
+package com.sashakhyzhun.wearexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.view.Gravity;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
